da2fa11547bfa9ca0df9f3c45237d5988e79d5cf
Christina <christinavhastenrath@gmail.com>
2020-01-27 09:31:10 -0800

    working on email

--- email.js
+++ email.js
@@ -10,6 +10,31 @@ sendmail({
     },
   ],
 }, (err, reply) => {
-  console.log(err && err.stack);
+  console.log('sendmail nodejs error', err && err.stack);
   console.dir(reply);
 });
+
+// const nodemailer = require('nodemailer');
+
+// const transport = nodemailer.createTransport({
+//   host: 'smtp-relay.gmail.com',
+//   port: 25,
+//   // auth: {
+//   //   user: 'put_your_username_here',
+//   //   pass: 'put_your_password_here',
+//   // },
+// });
+
+// const message = {
+//   from: 'christina.hastenrath@getpostman.com', // Sender address
+//   to: 'christina.hastenrath@postman.com', // List of recipients
+//   subject: 'Design Your Model S | Tesla', // Subject line
+//   text: 'Have the most fun you can in a car. Get your Tesla today!' // Plain text body
+// };
+// transport.sendMail(message, (err, info) => {
+//   if (err) {
+//     console.log('error sending email', err);
+//   } else {
+//     console.log('info sending email', info);
+//   }
+// });
--- logFile.sh
+++ logFile.sh
@@ -4,20 +4,21 @@ log=log_file.txt
 email=email.txt
  
 # create log file or overrite if already present
-printf "Log File - " > $log
+printf " " > $log
  
 # append git diff to log file
-git diff branch >> $log 
+git diff develop >> $log 
 
 # find H2 tag in diff
 result=$(grep -n '##' "$log")
 size=${#result}
+  echo "$size"
   if [ $size > 0 ]
   then
     # append data to email
     printf '%s\n' "${result[@]}" > $email
     # send mail to email address
-    node email.js
+    # node email.js
   else
     echo no data
   fi
